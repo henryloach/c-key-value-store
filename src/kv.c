ebb69dcd06fea6eca73eb8e187dc3fa92709f8fe
Christian Loach <=47648258+henryloach@users.noreply.github.com>
2026-09-23 19:07:08 +0100

    add kv_put implementation

--- src/kv.c
+++ src/kv.c
@@ -1,4 +1,64 @@
 #include <kv.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define TOMBSTONE 0x1
+
+size_t hash(char* val, int capacity) {
+    size_t hash = 0x13371337deadbeef;
+
+    while(*val) {
+        hash ^= *val;
+        hash = hash << 8;
+        hash += *val;
+
+        val++;
+    }
+
+    return hash % capacity;
+}
+
+int kv_put(kv_t* db, char* key, char* value) {
+    if (!db || !key || !value) return -1;
+
+    size_t idx = hash(key, db->capacity);
+
+    for (int i = 0; i < db->capacity - 1; i++) {
+
+        size_t real_idx = (idx + i) % db->capacity;
+
+        kv_entry_t* entry = &db->entries[real_idx];
+
+        // place in empty
+        if (!entry->key || entry->key == (void*)TOMBSTONE) {
+            char* newkey = strdup(key);
+            char* newval = strdup(value);
+            if (!newval || !newkey) {
+                free(newkey);
+                free(newval);
+                return -1;
+            }
+            entry->key = newkey;
+            entry->value = newval;
+
+            db->count++;
+            return real_idx;
+        }
+
+        // append
+        if (entry->key && entry->key != (void*)TOMBSTONE || !strcmp(entry->key, key)) {
+            char* newval = strdup(value);
+            if (!newval) return -1;
+            entry->value = newval;
+            return real_idx;
+        }
+
+        // continue
+
+    }
+
+    return -1;
+}
 
 kv_t* kv_init(size_t capacity) {
     if (capacity == 0) return NULL;
--- src/main.c
+++ src/main.c
@@ -2,9 +2,21 @@
 #include <kv.h>
 
 int main() {
-    kv_t* table = kv_init(3);
+    kv_t* table = kv_init(1024);
 
     printf("%p\n", table);
 
     printf("%ld\n", table->capacity);
+
+    printf("foo\n");
+
+    kv_put(table, "here", "I am");
+    kv_put(table, "here", "You are");
+
+    for (int i = 0; i < table->capacity; i++) {
+        kv_entry_t entry = table->entries[i];
+        if (entry.key) {
+            printf("%d - %s: %s\n", i, entry.key, entry.value);
+        }
+    }
 }
